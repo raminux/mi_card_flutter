52fd833a9e9ea6d6526fb1aac38c0fb749600d3d
Ramin Esmzad <ramin.esmzad@hotmail.com>
2022-06-20 22:54:36 +0430

    Hot reload only works with stateless or stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+   MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: Container(),
+      ),
+    );
+  }
+}
+
